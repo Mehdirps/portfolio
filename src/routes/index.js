7dfdc22c2c2c5f2ccd217790b301181d93c8d495
Mehdirps <mehdi.raposo77@gmail.com>
2023-01-22 18:00:50 +0100

    🎨 Dialogue page route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import IndexLayout from '../layouts/MainLayout';
+import Dialogue from '../pages/Dialogue';
 import Welcome from '../pages/Welcome';
 
 const index = () => {
@@ -9,6 +10,7 @@ const index = () => {
             <Routes>
                 <Route element={<IndexLayout />}>
                     <Route index element={<Welcome />} />
+                    <Route path='/presentation' element={<Dialogue />} />
                     <Route path='*' element={<Welcome />} />
                 </Route>
             </Routes>
