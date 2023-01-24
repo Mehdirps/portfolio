fe637ba1bce9fd9ad92bbed1767e34779c0cc28b
Mehdirps <mehdi.raposo77@gmail.com>
2023-01-24 10:07:01 +0100

    🎨 Dialogue

--- src/components/DialogueMessage.jsx
+++ src/components/DialogueMessage.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Icon } from '@iconify/react';
 
 const DialogueMessage = (props) => {
+    const message = props.message;
+    const index = useRef(0);
+    const [currentText, setCurrentText] = useState('');
+
+    useEffect(() => {
+        index.current = 0;
+        setCurrentText('');
+    }, [message]);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setCurrentText((value) => value + message.charAt(index.current));
+            index.current += 1;
+        }, 30);
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [currentText, message]);
     return (
         <div className="message-container" onClick={props.next}>
             <div className="message">
-                <p>{props.message}</p>
-                <Icon icon="material-symbols:arrow-drop-down-rounded" />
+                <p>{currentText}</p>
+                <Icon icon="material-symbols:arrow-drop-down-rounded" className='next'/>
             </div>
         </div>
     );
--- src/pages/Dialogue.jsx
+++ src/pages/Dialogue.jsx
@@ -1,34 +1,38 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DialogueMessage from '../components/DialogueMessage';
+import Loader from '../components/Loader';
 
 const Dialogue = () => {
     const [index, setIndex] = useState(0)
     const navigate = useNavigate();
 
     const dialogueMessage = [
-        "Bonjour ! Je m’appelle Mehdi Raposo. Je suis Développeur FullStack Web Et Mobile. J’ai un titre professionnel RNCP Designer Web - bac +2 et je suis actuellement en formation Concepteur Développeur d’applications - bac +3.",
-        "Dans le cadre de ma formation est en alternance,je recherche actuellement une entreprise dans le domaine du développement web ou mobile dans l’Yonne, l’Ile-de-France ou en 100% télétravail",
-        "J’ai des compétences en HTML, CSS, JavaScript, React.js, Node.js, Express, MongoDB et aussi en PHP avec le framework Symfony."
+        "  Bonjour ! Je m’appelle Mehdi Raposo. Je suis Développeur FullStack Web Et Mobile. J’ai un titre professionnel RNCP Designer Web - bac +2 et je suis actuellement en formation Concepteur Développeur d’applications - bac +3.",
+        " Dans le cadre de ma formation en alternance, je recherche actuellement une entreprise dans le domaine du développement web ou mobile dans l’Yonne, l’Ile-de-France ou en 100% télétravail.",
+        " J’ai des compétences en HTML, CSS, JavaScript et aussi en PHP."
     ]
     // TODO Ajouter toutes les phrases nécéssaires
     const NextMessage = () => {
         const increment = index + 1;
         if (increment === dialogueMessage.length) {
-            navigate('/')
+            navigate('/test')
             return
         }
         setIndex(increment)
     }
     return (
-        <section className='dialogue'>
-            <section className="container">
-                <figure className='personnage'>
-                    <img src="./img/personnage.png" alt="" />
-                </figure>
-                <DialogueMessage message={dialogueMessage[index]} next={NextMessage} />
+        <>
+            <Loader />
+            <section className='dialogue'>
+                <section className="container">
+                    <figure className='personnage'>
+                        <img src="./img/personnage.png" alt="" />
+                    </figure>
+                    <DialogueMessage message={dialogueMessage[index]} next={NextMessage} />
+                </section>
             </section>
-        </section>
+        </>
     );
 };
 
