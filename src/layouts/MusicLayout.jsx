2b39093cc012ddec7bfbfd33486413e089f432ca
Mehdirps <mehdi.raposo77@gmail.com>
2023-01-25 18:51:51 +0100

    🎨 Music player

--- src/layouts/MusicLayout.jsx
+++ src/layouts/MusicLayout.jsx
@@ -5,12 +5,26 @@ import { Outlet } from 'react-router-dom';
 const MusicLayout = () => {
     return (
         <>
-            <div className="music">
-                <Icon icon="material-symbols:music-video" className='icon' />
-                <audio controls loop>
-                    <source src="./music/pokemon-1.mp3" />
-                    <source src="./music/pokemon-2.mp3" />
-                </audio>
+            <div className="music-container">
+
+                <div className="music">
+                    <p>Musique 1</p>
+                    <div className="container">
+                        <Icon icon="material-symbols:music-video" className='icon' />
+                        <audio controls loop>
+                            <source src="./music/pokemon-1.mp3" />
+                        </audio>
+                    </div>
+                </div>
+                <div className="music2">
+                    <p>Musique 2</p>
+                    <div className="container">
+                        <Icon icon="material-symbols:music-video" className='icon' />
+                        <audio controls loop>
+                            <source src="./music/pokemon-2.mp3" />
+                        </audio>
+                    </div>
+                </div>
             </div>
             <Outlet />
         </>
