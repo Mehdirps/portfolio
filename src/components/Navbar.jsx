16a10ef39b32c585a90bf8c073428934bc09b9a2
Mehdirps <mehdi.raposo77@gmail.com>
2023-01-26 10:20:54 +0100

    🎨

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,34 +2,36 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className='navbar'>
-            <ul>
-                <li>
-                    <figure className='pokedex'>
-                        <img src="./img/icons/pokedex.png" alt="Icon Pokédex" />
-                    </figure>
-                    <p>Réadex</p>
-                </li>
-                <li>
-                    <figure className='pokeball'>
-                        <img src="./img/icons/pokeball.png" alt="Icon Pokéball" />
-                    </figure>
-                    <p>Skills Team</p>
-                </li>
-                <li>
-                    <figure className='dresseur'>
-                        <img src="./img/icons/dresseur.png" alt="Icon avatar" />
-                    </figure>
-                    <p>Profil</p>
-                </li>
-                <li>
-                    <figure className='cv'>
-                        <img src="./img/icons/cv.png" alt="Icon C.V" />
-                    </figure>
-                    <p>C.V</p>
-                </li>
-            </ul>
-        </nav>
+        <section className='header'>
+            <nav className='navbar'>
+                <ul>
+                    <li>
+                        <figure className='pokedex'>
+                            <img src="./img/icons/pokedex.png" alt="Icon Pokédex" />
+                        </figure>
+                        <p>Réadex</p>
+                    </li>
+                    <li>
+                        <figure className='pokeball'>
+                            <img src="./img/icons/pokeball.png" alt="Icon Pokéball" />
+                        </figure>
+                        <p>Skills Team</p>
+                    </li>
+                    <li>
+                        <figure className='dresseur'>
+                            <img src="./img/icons/dresseur.png" alt="Icon avatar" />
+                        </figure>
+                        <p>Profil</p>
+                    </li>
+                    <li>
+                        <figure className='cv'>
+                            <img src="./img/icons/cv.png" alt="Icon C.V" />
+                        </figure>
+                        <p>C.V</p>
+                    </li>
+                </ul>
+            </nav>
+        </section>
     );
 };
 
