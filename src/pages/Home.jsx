5477797d35a4fc242b1fb0aa836884169d61ac62
Mehdirps <mehdi.raposo77@gmail.com>
2023-01-26 10:30:16 +0100

    🎨 Début home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section className="home">
+            <div className="container">
+                
+            </div>
+        </section>
+    );
+};
+
+export default Home;
