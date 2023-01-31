ae755efc4573c98a8d5b8f2a8cfc9d1abe2cc9aa
Mehdirps <mehdi.raposo77@gmail.com>
2023-01-31 11:38:59 +0100

    🎨 duration

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -3,10 +3,11 @@ export const portfolioData = [
     id: 11,
     name: 'Foxinema',
     languages: ['tout', 'javascript', 'react.js', 'css', 'node.js'],
-    languagesIcons: ['fab fa-js', 'fab fa-react', 'fa-brands fa-node-js','fab fa-css3-alt', 'fab fa-sass'],
+    languagesIcons: ['fab fa-js', 'fab fa-react', 'fa-brands fa-node-js', 'fab fa-css3-alt', 'fab fa-sass'],
     source: '',
     info: 'Dans le cadre de ma seconde formation de Concepteur Développeur d\'Applications, j\'ai pour objectif de développer une application avec les technologies de mon choix. J\'ai donc choisi de créer une application de type forum où l\'on pourrai parler des films et séries que nous aimons à travers des postes et groupes de discussions, la possibilité d\'avoir des films et séries favoris etc.. Ayant pour objectif de devenir Développeur FullStack Javascript j\'ai décidé d\'utiliser Vue.js pour le coté front-end et Node.js avec Express et Moogouse pour le coté back-end',
-    picture: './media/foxinema.png'
+    picture: './media/foxinema.png',
+    realisationDuration: "En cours"
   },
   {
     id: 10,
@@ -15,7 +16,8 @@ export const portfolioData = [
     languagesIcons: ['fab fa-js', 'fab fa-vuejs'],
     source: 'https://gitlab.com/ofp-oc-cda/mehdirps/projet-vue',
     info: 'Après avoir appris les bases de PHP et Symfony, j\'ai décidé d\'apprendre les frameworks Frond Javascript. J\'ai donc suivi un tuto pour apprendre Vue.js dans lequel j\'ai créer une application web afin d\'apprendre les bases (router, composants etc..) puis j\'ai travailler avec une API pour apprendre à faire les relations avec Axios (fetch)',
-    picture: './media/repro-uber.png'
+    picture: './media/repro-uber.png',
+    realisationDuration: "1 semaine"
   },
   {
     id: 9,
@@ -24,7 +26,8 @@ export const portfolioData = [
     languagesIcons: ['fab fa-php', 'fab fa-bootstrap'],
     source: 'https://github.com/Mehdirps/E-commerce-Symfony6',
     info: 'Suite à mon apprentissage du POO, je souhaitais beaucoup apprendre le Frameworks PHP : Symfony. Utilisé par de nombreuses entreprises pour le web et afin de devenir Développeur FullStack, je me devais d\'apprendre à utiliser ce frameworks. En suivant un tuto youtube, j\'ai commencé à créer un site e-commerce (projet non terminé) basé sur Symfony et Twig',
-    picture: './media/e-commerce-SF6.png'
+    picture: './media/e-commerce-SF6.png',
+    realisationDuration: "1 mois"
   },
   {
     id: 8,
@@ -33,7 +36,8 @@ export const portfolioData = [
     languagesIcons: ['fab fa-php', 'fab fa-bootstrap'],
     source: 'https://github.com/Mehdirps/Petites-annonces-POO',
     info: 'Après avoir fini ma formation et obtenu mon diplôme j\'ai approfondi mes compétences en PHP en suivant un tuto Youtube sur le PHP orienté object. Le but était de créer un site d\'annonces type "Leboncoin" en POO et MVC.',
-    picture: './media/petites-annonces-POO.png'
+    picture: './media/petites-annonces-POO.png',
+    realisationDuration: "2 semaines"
   },
   {
     id: 7,
@@ -42,7 +46,8 @@ export const portfolioData = [
     languagesIcons: ['fab fa-wordpress', 'fab fa-css3-alt'],
     source: '',
     info: 'Afin de valider ma formation et de pouvoir passer l\'examen du titre professionnel, je devais effectuer un stage en entreprise d\'une durée de 2 mois. Durant ce stage, j\'ai fait la refonte totale d\'un site e-commerce d\'une boutique de cigarettes électronique. Et d\'une maquette graphique que j\'ai créer puis présentée et validée par mon tuteur de stage. Pour réaliser le site j\'ai choisi d\'utiliser le CMS WordPress avec WooCommerce et Elementor pour sa faciliter d\'intégration.',
-    picture: './media/vapinlove.png'
+    picture: './media/vapinlove.png',
+    realisationDuration: "3 mois"
   },
   {
     id: 6,
@@ -51,7 +56,8 @@ export const portfolioData = [
     languagesIcons: ['fab fa-css3-alt'],
     source: 'https://github.com/Mehdirps/Integration-sport',
     info: 'Projet d\'entrainement aux HTML et CSS durant ma formation, site réalisé sur la base d\'une maquette graphique fournie par mon formateur. Site réalisé seul',
-    picture: './media/allstar.png'
+    picture: './media/allstar.png',
+    realisationDuration: "4 jours"
   },
   {
     id: 5,
@@ -60,7 +66,8 @@ export const portfolioData = [
     languagesIcons: ['fab fa-css3-alt'],
     source: '',
     info: 'En vue de l\'examen pour mon titre professionnel, nous avons eu un examen blanc. Les consignes nous ont été données en conditions réelles d\'exemen, pdf expliquant les objectifs, maquettes à reproduire et tout fichiers (images etc..) nous ont été fourni. L\'objectif était de finir le projet en 2h',
-    picture: './media/examen.png'
+    picture: './media/examen.png',
+    realisationDuration: "3 heures"
   },
   {
     id: 4,
@@ -69,16 +76,18 @@ export const portfolioData = [
     languagesIcons: ['fab fa-css3-alt'],
     source: 'https://github.com/Mehdirps/Vincent-Pizza',
     info: 'Projet d\'entrainement aux HTML et CSS durant ma formation, site réalisé sur la base d\'une maquette graphique fournie par mon formateur. Site réalisé seul',
-    picture: './media/pizza.png'
+    picture: './media/pizza.png',
+    realisationDuration: "4 mois"
   },
   {
     id: 3,
     name: 'La télé de l\'Yonne',
     languages: ['tout', 'javascript', 'css', 'php'],
     languagesIcons: ['fab fa-js', 'fab fa-php', 'fab fa-css3-alt'],
-    source:'',
+    source: '',
     info: 'Voici le plus gros projet de ma formation, en lien avec l\'association "La Télé de l\'Yonne" une web TV icaunaise. Nous avions pour mission de faire une refonte totale de leur site web avec carte blanche pour la charte graphique et le design. Les téchnologies utlisées pour ce projet sont HTML, CSS, JavaScript et PHP.',
-    picture: './media/Tv-Yonne.png'
+    picture: './media/Tv-Yonne.png',
+    realisationDuration: "5 mois"
   },
   {
     id: 2,
@@ -87,15 +96,17 @@ export const portfolioData = [
     languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
     source: 'https://github.com/Mehdirps/ACS-Music-Pascal-Mehdi',
     info: 'Cette application web fonctionne avec une API contenant des musiques, albums et artistes. Le but était de faire communiquer cette API avec notre application avec JavaScript. Affichage des albums, musiques et artistes, inscriptions et authentifications JWT. Nous avions aussi eu pour mission la création d\'un Logo. Ce projet a été réalisé en duo',
-    picture: './media/PadisMusic.png'
+    picture: './media/PadisMusic.png',
+    realisationDuration: "1 mois"
   },
   {
-    id:1,
+    id: 1,
     name: 'Saum Hotel',
     languages: ['tout', 'javascript', 'css'],
     languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
     source: 'https://github.com/Mehdirps/site_hotel-M.G',
     info: 'Création d\'un site de réservation d\'hôtel fictif dans le cadre de ma formation Designer Web en duo. Le site a été fait avec HTML, CSS et Javascript. Le projet devait communiquer avec une base de données MySql via PHP mais nous n\'avons pas eu le temps nécessaire pour finir le projet.',
-    picture: './media/saumhotel.png'
+    picture: './media/saumhotel.png',
+    realisationDuration: "2 semaines"
   },
 ]
