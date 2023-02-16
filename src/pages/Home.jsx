a3e05aaffa288e82e8e76aab2f48cbe109c9fcfc
Mehdirps <mehdi.raposo77@gmail.com>
2023-02-16 16:52:51 +0100

    🎨 devCard component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Ball from '../components/Ball';
+import DevCard from '../components/DevCard';
 import Readex from '../components/Readex';
 
 const Home = () => {
     return (
         <section className="home">
             <Readex />
+            <DevCard />
             <Ball />
         </section >
     );
