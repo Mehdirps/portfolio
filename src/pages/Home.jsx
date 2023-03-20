5a66edf09f533c9d5c0d9c1224e237b664c6741a
Mehdirps <mehdi.raposo77@gmail.com>
2023-03-20 12:20:41 +0100

    🎨 Game component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Ball from '../components/Ball';
 import DevCard from '../components/DevCard';
+import Game from '../components/Game';
 import Readex from '../components/Readex';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Readex />
             <DevCard />
             <Ball />
+            <Game />
         </section >
     );
 };
