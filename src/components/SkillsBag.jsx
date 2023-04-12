1a6265c7087afece5e8cbf0cf7f037555a5638ee
Mehdirps <mehdi.raposo77@gmail.com>
2023-04-12 16:28:43 +0200

    🎨 Skills bag header

---
+++ src/components/SkillsBag.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import SkillsBagHeader from './skillsBag/SkillsBagHeader';
+import SkillsBagBody from './skillsBag/SkillsBagBody';
+
+const SkillsBag = () => {
+    const open = useSelector((state) => state.skillsBag.open)
+
+    const skillsList = [
+        {
+            'name': 'HTML',
+            'icon': './img/icons/skills/front/html.png',
+            'started_date': '06/2021',
+            'category': 'front-end'
+        },
+        {
+            'name': 'CSS',
+            'icon': './img/icons/skills/front/css.png',
+            'started_date': '06/2021',
+            'category': 'front-end'
+        },
+        {
+            'name': 'Sass',
+            'icon': './img/icons/skills/front/sass.png',
+            'started_date': '08/2021',
+            'category': 'front-end'
+        },
+        {
+            'name': 'JavaScript',
+            'icon': './img/icons/skills/front/javascript.png',
+            'started_date': '07/2021',
+            'category': 'front-end'
+        },
+        {
+            'name': 'React.js',
+            'icon': './img/icons/skills/front/react.png',
+            'started_date': '11/2022',
+            'category': 'front-end'
+        },
+        {
+            'name': 'Redux',
+            'icon': './img/icons/skills/front/redux.png',
+            'started_date': '01/2023',
+            'category': 'front-end'
+        },
+        {
+            'name': 'Vue.js',
+            'icon': './img/icons/skills/front/vue.png',
+            'started_date': '07/2022',
+            'category': 'front-end'
+        },
+        {
+            'name': 'WordPress',
+            'icon': './img/icons/skills/front/wordpress.png',
+            'started_date': '08/2021',
+            'category': 'front-end'
+        },
+        {
+            'name': 'Node.js',
+            'icon': './img/icons/skills/back/node.png',
+            'started_date': '11/2022',
+            'category': 'back-end'
+        },
+        {
+            'name': 'Express',
+            'icon': './img/icons/skills/back/express.png',
+            'started_date': '11/2022',
+            'category': 'back-end'
+        },
+        {
+            'name': 'MongoDB',
+            'icon': './img/icons/skills/back/mongodb.png',
+            'started_date': '11/2022',
+            'category': 'database'
+        },
+        {
+            'name': 'PHP',
+            'icon': './img/icons/skills/back/php.png',
+            'started_date': '09/2021',
+            'category': 'back-end'
+        },
+        {
+            'name': 'Symfony',
+            'icon': './img/icons/skills/back/symfony.png',
+            'started_date': '03/2022',
+            'category': 'back-end'
+        },
+        {
+            'name': 'MySql',
+            'icon': './img/icons/skills/back/mysql.png',
+            'started_date': '09/2021',
+            'category': 'database'
+        },
+        {
+            'name': 'Vs Code',
+            'icon': './img/icons/skills/tools/vscode.png',
+            'started_date': '06/2021',
+            'category': 'gestion'
+        },
+        {
+            'name': 'Figma',
+            'icon': './img/icons/skills/tools/figma.png',
+            'started_date': '07/2021',
+            'category': 'design'
+        },
+        {
+            'name': 'FileZilla',
+            'icon': './img/icons/skills/tools/filezilla.png',
+            'started_date': '10/2021',
+            'category': 'gestion'
+        },
+        {
+            'name': 'Gimp',
+            'icon': './img/icons/skills/tools/gimp.png',
+            'started_date': '10/2021',
+            'category': 'design'
+        },
+        {
+            'name': 'Inkscape',
+            'icon': './img/icons/skills/tools/inkscape.png',
+            'started_date': '10/2021',
+            'category': 'design'
+        },
+        {
+            'name': 'Illustrator',
+            'icon': './img/icons/skills/tools/illustrator.png',
+            'started_date': '01/2022',
+            'category': 'design'
+        },
+        {
+            'name': 'PhotoShop',
+            'icon': './img/icons/skills/tools/photoshop.png',
+            'started_date': '01/2022',
+            'category': 'design'
+        },
+        {
+            'name': 'Postman',
+            'icon': './img/icons/skills/tools/postman.png',
+            'started_date': '11/2022',
+            'category': 'gestion'
+        },
+        {
+            'name': 'MongoDbCompass',
+            'icon': './img/icons/skills/back/mongodb.png',
+            'started_date': '11/2022',
+            'category': 'gestion'
+        },
+        {
+            'name': 'Git',
+            'icon': './img/icons/skills/tools/git.png',
+            'started_date': '07/2021',
+            'category': 'gestion'
+        },
+        {
+            'name': 'GitHub',
+            'icon': './img/icons/skills/tools/github.png',
+            'started_date': '07/2021',
+            'category': 'gestion'
+        },
+        {
+            'name': 'GitLab',
+            'icon': './img/icons/skills/tools/gitlab.png',
+            'started_date': '07/2022',
+            'category': 'gestion'
+        },
+    ]
+
+    return (
+        <>
+            {
+                open ?
+                    <section className='skills-bag'>
+                        <SkillsBagHeader SkillsNumber={skillsList.length} />
+                        <SkillsBagBody skills={skillsList} />
+                    </section>
+                    : ''
+            }
+        </>
+    );
+};
+
+export default SkillsBag;
--- src/components/skillsBag/SkillsBagHeader.jsx
+++ src/components/skillsBag/SkillsBagHeader.jsx
@@ -7,27 +7,56 @@ const SkillsBagHeader = (props) => {
     const dispatch = useDispatch();
 
     const handleCategory = (e) => {
-        dispatch(setCategory(e.target.attributes.category.value))
+        dispatch(setCategory(e.target.attributes.category.value));
     }
+
+    const categories = [
+        {
+            'name': 'front-end',
+            'src': './img/icons/skills/frontend.png',
+        },
+        {
+            'name': 'back-end',
+            'src': './img/icons/skills/backend.png',
+        },
+        {
+            'name': 'database',
+            'src': './img/icons/skills/database.png',
+        },
+        {
+            'name': 'design',
+            'src': './img/icons/skills/design.png',
+        },
+        {
+            'name': 'gestion',
+            'src': './img/icons/skills/gestion.png',
+        },
+
+    ]
+
     return (
         <section className='bag-header'>
-            <div className="title">
-                <figure className='bag'>
+            <section className="top-header">
+                <div className="title">
                     <img src="./img/icons/bag.png" alt="icon sac" />
-                </figure>
-                <h2>Skills Bag</h2>
-            </div>
-            <div className="category">
+                    <h2>Skills</h2>
+                </div>
+                <div className="skills-number">
+                    <img src="./img/icons/skills/skill.png" alt="" />
+                    <p>{props.SkillsNumber} Skills</p>
+                </div>
+            </section>
+            <section className="category">
                 <p>{category}</p>
                 <div className="category-icons">
-                    <img src="./img/icons/skills/frontend.png" alt="icon front-end" category="front-end" onClick={(e) => handleCategory(e)} />
-                    <img src="./img/icons/skills/backend.png" alt="icon back-end" category="back-end" onClick={(e) => handleCategory(e)} />
-                    <img src="./img/icons/skills/database.png" alt="icon base de donnée" category="database" onClick={(e) => handleCategory(e)} />
-                    <img src="./img/icons/skills/design.png" alt="icon design" category="design" onClick={(e) => handleCategory(e)} />
-                    <img src="./img/icons/skills/gestion.png" alt="icon gestion" category="gestion" onClick={(e) => handleCategory(e)} />
+                    {
+                        categories.map((item, id) =>
+                            <img src={item.src} alt={`icon ` + item.name} category={item.name} onClick={(e) => handleCategory(e)} className={item.name === category ? 'active' : ''} key={id} />
+                        )
+                    }
                 </div>
-            </div>
-            <p className='skills-numer'>{props.SkillsNumber} Skills</p>
+            </section>
+
         </section>
     );
 };
