b604afcb0972674cb8efed3d70ad88db34a3ed3a
Mehdirps <mehdi.raposo77@gmail.com>
2023-04-12 15:15:23 +0200

    🎨 SkillsBag Store

--- src/stores/SkillsBagSlice.js
+++ src/stores/SkillsBagSlice.js
@@ -3,14 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 const SkillsBagSlice = createSlice({
     name: 'skillsBag',
     initialState: {
-        open: false,
-
+        open: true,
+        category: 'front-end',
+        skillsList: [],
+        skill: [],
     },
     reducers: {
         setSkillsBag: (state, action) => {
             state.open = action.payload
+        },
+        setCategory: (state, action) => {
+            state.category = action.payload
+        },
+        setSkill: (state, action) => {
+            state.skill = action.payload
         }
     }
 });
-export const { setSkillsBag } = SkillsBagSlice.actions;
+export const { setSkillsBag, setCategory, setSkill } = SkillsBagSlice.actions;
 export default SkillsBagSlice.reducer;
