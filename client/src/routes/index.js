86652d8af4768cd8971acf4bcd69b354986e4c5f
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-11 18:50:13 +0200

    🐛 fix : delete test page

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -4,7 +4,6 @@ import AppLayout from '../layouts/AppLayout';
 import Layout from '../layouts/Layout';
 import Dialogue from '../pages/Dialogue';
 import Welcome from '../pages/Welcome';
-import Test from '../pages/test';
 import Home from '../pages/Home';
 import MusicLayout from '../layouts/MusicLayout';
 
@@ -17,7 +16,6 @@ const index = () => {
                     {/* <Route element={<AppLayout />}> */}
                     <Route index element={<Welcome />} />
                     <Route path='/presentation' element={<Dialogue />} />
-                    <Route path='/test' element={<Test />} />
                     <Route element={<Layout />}>
                         <Route path='/accueil' element={<Home />} />
                     </Route>
