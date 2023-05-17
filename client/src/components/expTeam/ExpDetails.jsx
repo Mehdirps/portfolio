f984eb81f0c61820d202b8033c3a4bfc1788886e
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-17 11:06:35 +0200

    🎨 up : ExpDetails

---
+++ client/src/components/expTeam/ExpDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const ExpDetails = () => {
+
+    const exp = useSelector((state) => state.expTeam.exp);
+
+    return (
+        <section className="exp-details">
+            <figure className='exp-details-logo'>
+                <img src={exp.logo} alt={`Illustration du logo de l'entreprise ${exp.entreprise}`} />
+            </figure>
+            <p className='exp-desc'>{exp.desc}</p>
+        </section>
+    );
+};
+
+export default ExpDetails;
