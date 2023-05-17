490dc39abe882f5cc5e763a7067464cd3b65d116
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-17 11:06:10 +0200

    🎨 up : ExpTeam components

---
+++ client/src/components/ExpTeam.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCategory, setList, setListLength } from '../stores/ExpTeamSlice';
+import ExpList from './expTeam/ExpList';
+import ExpDetails from './expTeam/ExpDetails';
+import { ExpericesPro, ExpericesSco } from '../data/portfolioData';
+
+
+const ExpTeam = () => {
+    const open = useSelector((state) => state.expTeam.open);
+    const expCat = useSelector((state) => state.expTeam.category);
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            {
+                open ?
+                    <section className="expTeam">
+                        <section className="expTeam-header">
+                            <div className="title">
+                                <img src="./img/icons/empty-pokeball.png" alt="Icon d'une pokemon" />
+                                <h2>Éxperiences</h2>
+                            </div>
+                            <div className="category-btns">
+                                <p onClick={() => {
+                                    dispatch(setCategory('pro'));
+                                    dispatch(setList(ExpericesPro));
+                                    dispatch(setListLength(ExpericesPro.length))
+                                }} className={expCat === 'pro' ? 'selected' : null}>Pro</p>
+                                <p onClick={() => {
+                                    dispatch(setCategory('education'));
+                                    dispatch(setList(ExpericesSco));
+                                    dispatch(setListLength(ExpericesSco.length))
+                                }} className={expCat === 'education' ? 'selected' : null}>Formation</p>
+                            </div>
+                        </section>
+                        <ExpList />
+                        <ExpDetails />
+                    </section>
+                    : null
+            }
+        </>
+    );
+};
+
+export default ExpTeam;
