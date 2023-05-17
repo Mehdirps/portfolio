4c9eb98506d7a76de068812a39a3ab8cfec1353f
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-17 11:06:43 +0200

    🎨 up : ExpList

---
+++ client/src/components/expTeam/ExpList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ExpCard from './ExpCard';
+import { setExp } from '../../stores/ExpTeamSlice';
+
+const ExpList = () => {
+    const expList = useSelector((state) => state.expTeam.list);
+    const listLength = useSelector((state) => state.expTeam.listLength);
+    console.log(listLength);
+    let emptyExp = useMemo(() => [], []);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setExp(expList[0]));
+
+    });
+
+    if (listLength > 0) {
+        const noExp = 6 - listLength;
+        emptyExp = [];
+        for (let i = 0; i < noExp; i++) {
+            emptyExp.push(<article className="empty-exp" key={i}></article>)
+
+        }
+    }
+
+    return (
+        <section className="exp-list-container">
+            {
+                expList.map((item, id) =>
+                    <ExpCard key={id} item={item} />
+                )
+            }
+            {emptyExp}
+        </section>
+    );
+};
+
+export default ExpList;
