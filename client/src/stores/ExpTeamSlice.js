5c968ebe999bc3ce91e28f52b09c0827280fdd30
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-17 11:05:24 +0200

    🎨 up : expTeam Stores

--- client/src/stores/ExpTeamSlice.js
+++ client/src/stores/ExpTeamSlice.js
@@ -1,16 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { ExpericesPro } from '../data/portfolioData';
 
 const ExpTeamSlice = createSlice({
     name: 'ExpTeam',
     initialState: {
         open: true,
+        category: 'pro',
+        list: ExpericesPro,
+        listLength: ExpericesPro.length,
+        exp: {},
     },
     reducers: {
         setExpTeam: (state, action) => {
             state.open = action.payload
         },
-
+        setCategory: (state, action) => {
+            state.category = action.payload
+        },
+        setList: (state, action) => {
+            state.list = action.payload
+        },
+        setListLength: (state, action) => {
+            state.listLength = action.payload
+        },
+        setExp: (state, action) => {
+            state.exp = action.payload
+        }
     }
 });
-export const { setExpTeam } = ExpTeamSlice.actions;
+export const { setExpTeam, setCategory, setList, setListLength, setExp } = ExpTeamSlice.actions;
 export default ExpTeamSlice.reducer;
