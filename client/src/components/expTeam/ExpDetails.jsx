0ee47659525b415f8fd13dc447f760a7be969020
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-24 15:10:00 +0200

    🎨 up : added experience poste

--- client/src/components/expTeam/ExpDetails.jsx
+++ client/src/components/expTeam/ExpDetails.jsx
@@ -4,12 +4,13 @@ import { useSelector } from 'react-redux';
 const ExpDetails = () => {
 
     const exp = useSelector((state) => state.expTeam.exp);
-
+    
     return (
         <section className="exp-details">
             <figure className='exp-details-logo'>
                 <img src={exp.logo} alt={`Illustration du logo de l'entreprise ${exp.entreprise}`} />
             </figure>
+            <h3>{exp.poste}</h3>
             <p className='exp-desc'>{exp.desc}</p>
         </section>
     );
