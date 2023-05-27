450f2124f8148e3f8bb91fed8bad289e6302c583
Mehdirps <mehdi.raposo77@gmail.com>
2023-05-27 08:24:06 +0200

    🐛 fix : link in all page

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -6,29 +6,29 @@ const Welcome = () => {
 
     return (
         <>
-            <section className='welcome-page'>
-                <div className="container">
-                    <div className="container-img">
-                        <figure className="title">
-                            <img src="./img/title.png" alt="Illustration du titre du site" />
-                        </figure>
-                        <figure className="logo">
-                            <img src="./img/Logo.svg" alt="Logo du site" />
-                        </figure>
-                    </div>
-                    <div className="next">
-                        <NavLink exact="true" to='/presentation'>
+            <NavLink exact="true" to='/presentation'>
+                <section className='welcome-page'>
+                    <div className="container">
+                        <div className="container-img">
+                            <figure className="title">
+                                <img src="./img/title.png" alt="Illustration du titre du site" />
+                            </figure>
+                            <figure className="logo">
+                                <img src="./img/Logo.svg" alt="Logo du site" />
+                            </figure>
+                        </div>
+                        <div className="next">
                             <p>Appuyer pour continuer</p>
-                        </NavLink>
+                        </div>
+                        <div className="copyright">
+                            <Icon icon="material-symbols:copyright-outline" />
+                            <p><span className='year'>2023</span> Mehdi Raposo.</p>
+                        </div>
                     </div>
-                    <div className="copyright">
-                        <Icon icon="material-symbols:copyright-outline" />
-                        <p><span className='year'>2023</span> Mehdi Raposo.</p>
-                    </div>
-                </div>
-                <div className="background1"></div>
-                <div className="background2"></div>
-            </section>
+                    <div className="background1"></div>
+                    <div className="background2"></div>
+                </section>
+            </NavLink>
         </>
     );
 };
